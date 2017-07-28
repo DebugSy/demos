efba66768dd780a4bf8ce787a24f41828871231c
DebugSy <532151398@qq.com>
2017-07-28 10:50:05 +0800

    init project

---
+++ src/main/java/com/shiy/springmvc/Application.java
@@ -0,0 +1,12 @@
+package com.shiy.springmvc;
+
+/**
+ * Created by DebugSy on 2017/7/28.
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+
+}
