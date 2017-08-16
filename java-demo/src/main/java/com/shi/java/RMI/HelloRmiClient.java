0a247a7579fe4e5746eedae8e0ee5fe4b506abbb
DebugSy <532151398@qq.com>
2017-08-16 14:55:27 +0800

    Java RMI 测试代码

---
+++ java-demo/src/main/java/com/shi/java/RMI/HelloRmiClient.java
@@ -0,0 +1,34 @@
+package com.shi.java.RMI;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+/**
+ * Created by DebugSy on 2017/8/16.
+ */
+public class HelloRmiClient {
+
+	IntHelloRmi helloRmi;
+	public void client() throws RemoteException,MalformedURLException,NotBoundException {
+		helloRmi=(IntHelloRmi) Naming.lookup("rmi://localhost:9999/HelloRmi");
+		System.out.println("client:");
+		System.out.println(helloRmi.helloRmi());
+		System.out.println(helloRmi.sayHello("yiyong"));
+	}
+
+	public static void main(String[] args) {
+		HelloRmiClient client = new HelloRmiClient();
+		try {
+			client.client();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
