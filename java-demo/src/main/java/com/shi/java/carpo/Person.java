894a0c888cc78bc8d5a23d447a6d603f11f8396c
DebugSy <532151398@qq.com>
2017-08-29 10:24:04 +0800

    carpo测试数据生成

---
+++ java-demo/src/main/java/com/shi/java/carpo/Person.java
@@ -0,0 +1,49 @@
+package com.shi.java.carpo;
+
+import java.io.Serializable;
+
+/**
+ * Created by DebugSy on 2017/8/28.
+ */
+public class Person implements Serializable{
+
+	private long id;
+
+	private String name;
+
+	private int age;
+
+	public Person(long id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public static Person builer(long id, String name, int age){
+		return new Person(id, name, age);
+	}
+}
---
+++ java-demo/src/test/java/com/shi/java/java/GenerateTestDataTest.java
@@ -0,0 +1,18 @@
+package com.shi.java.java;
+
+import org.junit.Test;
+
+/**
+ * Created by DebugSy on 2017/8/28.
+ */
+public class GenerateTestDataTest {
+
+	@Test
+	public void testData(){
+		int i = 1;
+		char c;
+		c = (char)(97 + i);
+		System.out.println(c);
+	}
+
+}
