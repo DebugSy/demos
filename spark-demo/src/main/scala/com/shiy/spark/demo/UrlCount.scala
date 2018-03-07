96712202471510a52339f91a649aa483a51c4b3e
DebugSy <532151398@qq.com>
2018-03-07 23:13:07 +0800

    url分组统计排序

--- spark-demo/src/main/scala/com/shiy/spark/demo/UrlCount.scala
+++ spark-demo/src/main/scala/com/shiy/spark/demo/UrlCount.scala
@@ -1,16 +1,35 @@
 package com.shiy.spark.demo
 
+import java.net.URL
+
 import org.apache.spark.{SparkConf, SparkContext}
 
 object UrlCount {
 
   def main(args: Array[String]): Unit = {
+
+    val subjects = Array("java.itcast.cn", "net.itcast.cn", "php.itcast.cn")
+
     val sparkConf = new SparkConf().setAppName("UrlCount").setMaster("local")
     val sc = new SparkContext(sparkConf)
 
     val urlData = sc.textFile("D:/tmp/data/spark/test_data/itcast.log")
-    val result = urlData.map(_.split("\t")).map(x => (x(1).substring(7, x(1).indexOf('.')), 1)).reduceByKey(_ + _)
-    result.collect().map(print(_))
+    val rdd1 = urlData.map(_.split("\t")).map(x => (x(1), 1)).reduceByKey(_ + _)
+
+    val rdd2 = rdd1.map(t => {
+      val url = t._1
+      val host = new URL(url).getHost
+      (host, url, t._2)
+    })
+//    rdd2.collect().map(println(_))
+
+    subjects.foreach(subject => {
+      val rdd3 = rdd2.filter(_._1 == subject).sortBy(_._3, false).take(3)
+      rdd3.map(println(_))
+      println("***************************")
+    })
+
+
     sc.stop()
 
   }
