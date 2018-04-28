414c0bd3e4dfe0aaa2026ceafaf695b0403360be
DebugSy <532151398@qq.com>
2018-04-28 09:59:14 +0800

    [scala函数式编程] 4.x练习

--- scala-demo/src/main/scala/com/shiy/scala/book/errorhandling/Option.scala
+++ scala-demo/src/main/scala/com/shiy/scala/book/errorhandling/Option.scala
@@ -36,4 +36,45 @@ object Option {
     else Some(xs.sum / xs.length)
   }
 
+  def variance(xs: Seq[Double]): Option[Double] = {
+   mean(xs) flatMap (m => mean(xs map (x => math.pow(x - m, 2))))
+  }
+
+  //练习4.3
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] = {
+    a flatMap (aa => (b map (bb => f(aa, bb))))
+  }
+
+  //for推导实现map2
+  def map2for[A,B,C](a: Option[A],b: Option[B])(f: (A,B) => C): Option[C] = {
+    for {
+      aa <- a
+      bb <- b
+    }yield f(aa, bb)
+  }
+
+  //练习4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+    }
+
+  //练习4.5
+  def sequence_1[A](a: List[Option[A]]): Option[List[A]] = {
+    a.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
+  }
+
+
+  def traverse[A,B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+    }
+  }
+
+  //练习4.6
+  //练习4.7
+  //练习4.8
+
 }
