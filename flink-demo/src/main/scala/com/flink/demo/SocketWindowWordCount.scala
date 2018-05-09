0403024f608dcbe6f26dbdf0880a322616b94819
DebugSy <532151398@qq.com>
2018-05-09 10:46:20 +0800

    [flink] run flink socketWindowWordCount in local

--- flink-demo/src/main/scala/com/flink/demo/SocketWindowWordCount.scala
+++ flink-demo/src/main/scala/com/flink/demo/SocketWindowWordCount.scala
@@ -1,6 +1,8 @@
 package com.flink.demo
 
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 
@@ -13,18 +15,20 @@ object SocketWindowWordCount {
 
   def main(args: Array[String]): Unit = {
 
-    val port: Int = try {
-      ParameterTool.fromArgs(args).getInt("port")
-    } catch {
-      case e: Exception => {
-        System.err.println("No port specified. Please run 'SocketWindowWordCount --port <port>'")
-        return
-      }
-    }
+//    implicit val wordWithCountInfo = TypeInformation.of(classOf[WordWithCount])
 
-    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+//    val port: Int = try {
+//      ParameterTool.fromArgs(args).getInt("port")
+//    } catch {
+//      case e: Exception => {
+//        System.err.println("No port specified. Please run 'SocketWindowWordCount --port <port>'")
+//        return
+//      }
+//    }
 
-    val text = env.socketTextStream("localhost", port, '\n')
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI()
+
+    val text = env.socketTextStream("localhost", 9000, '\n')
 
     val windowCounts = text.flatMap(w => w.split("\\s"))
       .map( w => WordWithCount(w, 1))
@@ -33,6 +37,8 @@ object SocketWindowWordCount {
       .sum("count")
 
     windowCounts.print().setParallelism(1)
+
+    env.execute("Socket Window WordCount")
 
   }
 
