6ea8a9365364d2a77e30e5e024ed259529405de4
DebugSy <532151398@qq.com>
2018-07-16 18:09:02 +0800

    [spring-data-jpa] 使用Criteria方式自定义查询

--- spring-data-demo/src/test/java/com/shiy/spring/data/UserRepositoryTest.java
+++ spring-data-demo/src/test/java/com/shiy/spring/data/UserRepositoryTest.java
@@ -2,18 +2,24 @@ package com.shiy.spring.data;
 
 import com.shiy.spring.jpa.data.UserRepository;
 import com.shiy.spring.jpa.data.User;
+import org.h2.expression.ExpressionColumn;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=JpaConfig.class)
 public class UserRepositoryTest {
 
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     private UserRepository userRepository;
@@ -30,6 +36,16 @@ public class UserRepositoryTest {
     public void testFindByName(){
         User user = userRepository.findOneByName(1, "a");
         System.out.println(user);
+    }
+
+    @Test
+    public void testCriteriaQuery(){
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> root = cq.from(User.class);
+        cq.where(cb.equal(root.get("name"), "a"));
+        TypedQuery<User> query = entityManager.createQuery(cq);
+        System.out.println(query.getResultList());
     }
 
 }
